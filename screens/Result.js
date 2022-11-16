f0c846db865c2d0b0f4a9a6891e0514d86d77b5f
Utkarsh Adhikrao Kamble <utkarsh1172@gmail.com>
2022-11-16 13:30:35 +0530

    second commit

--- screens/Result.js
+++ screens/Result.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
 
 const Result = () => {
   return (
@@ -13,6 +13,11 @@ const Result = () => {
             uri:'https://i.pinimg.com/736x/6a/7b/d3/6a7bd354f2459e471f72fee63e46e716.jpg'
           }}
         />
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Text>Home</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
